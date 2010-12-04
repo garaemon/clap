cf08c9a4758a7db998a7b26cbbe57f984bf5a658
g000001 <chiba.masaomi@gmail.com>
2010-12-04 16:34:30 +0900

    Fixed: typo

--- src/builtin/util.lisp
+++ src/builtin/util.lisp
@@ -2,6 +2,6 @@
 
 (defmacro defconstant* (symbol value &optional doc)
   `(defconstant ,symbol (if (boundp ',symbol)
-                            (symbolbol-value ',symbol)
+                            (symbol-value ',symbol)
                             ,value)
      ,@(when doc (list doc))))
