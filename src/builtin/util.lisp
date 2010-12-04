4cf153345599c579d2c2d59f0f12628dddff59f6
g000001 <chiba.masaomi@gmail.com>
2010-12-04 19:05:41 +0900

    Add: sconc (string concatenate)

--- src/builtin/util.lisp
+++ src/builtin/util.lisp
@@ -5,3 +5,24 @@
                             (symbol-value ',symbol)
                             ,value)
      ,@(when doc (list doc))))
+
+;; borrow from TAO
+(defun sconc (&rest strings)
+  "(sconc \"a\" \"b\") -> \"ab\"
+   (sconc \"123\" \"45\" \"6789\") -> \"123456789\"
+   (sconc \"abc\" nil) -> \"abc\""
+  (declare (optimize (safety 0) (speed 3))
+           (dynamic-extent strings))
+  (let ((len 0)
+        (pos 0))
+    (declare (fixnum len pos))
+    (dolist (s strings)
+      (declare (simple-string s))
+      (incf len (length s)))
+    (let ((result (make-string len)))
+      (declare (simple-string result))
+      (dolist (s strings)
+        (declare (simple-string s))
+        (loop :for c :across s
+              :do (setf (schar result pos) c) (incf pos)))
+      result)))
