32d556bedaf4b072fb2d40c68d5e1c335c8d5b59
garaemon <garaemon@gmail.com>
2010-12-06 22:09:11 +0900

    add documentation

--- src/builtin/clap-builtin.lisp
+++ src/builtin/clap-builtin.lisp
@@ -2,7 +2,7 @@
 (defpackage :clap-builtin
   (:use #:common-lisp)
   (:shadow #:abs)
-  (:documentation "fill this documentation")
+  (:documentation "this package is an implementation of __builtin__ package")
   (:export #:range
            #:all
            #:any
