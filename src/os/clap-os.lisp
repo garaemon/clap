6387a0ffcf3f728d1dadadf5f3cc170300b89d6c
g000001 <chiba.masaomi@gmail.com>
2010-12-25 19:26:49 +0900

    uname -aした文字列を分解するのが面倒だったので、個別に5回実行することにしたらどうも遅いらしい

--- src/os/clap-os.lisp
+++ src/os/clap-os.lisp
@@ -1,4 +1,33 @@
-
 (defpackage :clap-os
   (:use #:common-lisp)
-  (:documentation "fill this documentation"))
+  (:documentation "fill this documentation")
+  (:export :uname))
+
+(in-package :clap-os)
+
+(defun uname ()
+  "(values sysname nodename release version machine)"
+  (flet (#+sbcl
+         (uname (arg)
+           (let* ((process (sb-ext:run-program "uname"
+                                               (list arg)
+                                               :input nil
+                                               :output :stream
+                                               :search T))
+                  (output (read-line (sb-impl::process-output process) nil)))
+             (close (sb-impl::process-output process))
+             (string-right-trim clap-string:+whitespace+
+                                output)))
+         #-sbcl
+         (uname (arg) arg))
+    (values
+     ;; sysname
+     (uname "-s")
+     ;; nodename
+     (uname "-n")
+     ;; release
+     (uname "-r")
+     ;; version
+     (uname "-v")
+     ;; machine
+     (uname "-m"))))
