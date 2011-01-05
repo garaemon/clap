83dd01bbbb7641d8cb3b06b22d91b2e033ecb5c4
garaemon <garaemon@gmail.com>
2011-01-05 22:45:18 +0900

    add the documentation strings

--- src/string/constants.lisp
+++ src/string/constants.lisp
@@ -4,51 +4,68 @@
 ;; http://docs.python.org/release/2.4/lib/node104.html
 
 (defconstant* +ascii-lowercase+
-  "abcdefghijklmnopqrstuvwxyz")
+  "abcdefghijklmnopqrstuvwxyz"
+  "a string composed of the ASCII lowercase characters.")
 
 (defconstant* +ascii-uppercase+
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  "a string composed of the ASCII uppercase characters.")
 
 (defconstant* +ascii-letters+
   #.(concatenate 'string
                  +ascii-lowercase+
-                 +ascii-uppercase+))
+                 +ascii-uppercase+)
+  "a string composed of the ASCII alphabet characters.")
 
 (defconstant* +digits+
-  "0123456789")
+  "0123456789"
+  "a string composed of the digit characters")
 
 (defconstant* +hexdigits+
-  "0123456789abcdefABCDEF")
+  "0123456789abcdefABCDEF"
+  "a string composed of the hexdigit characters")
 
 (defconstant* +octdigits+
-  "01234567")
+  "01234567"
+  "a string composed of the octdigit characters")
 
 ;; FIXME -> locale
 (defconstant* +lowercase+
-  "abcdefghijklmnopqrstuvwxyz")
+  "abcdefghijklmnopqrstuvwxyz"
+  "a string composed of the lowercase characters. currently locale
+is not supported.")
 
 ;; FIXME -> locale
 (defconstant* +uppercase+
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  "a string composed of the uppercase characters. currently locale
+is not supported.")
 
 ;; FIXME -> locale
 (defconstant* +punctuation+
-  "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~")
+  "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+  "a string composed of the punctuation characters. currently locale
+is not supported.")
 
 ;; whitespace
 ;; http://www.lispworks.com/documentation/HyperSpec/Body/26_glo_w.htm#whitespace
 (defconstant* +whitespace+
   #.(format nil
             "~{~C~}"
-            clap-builtin:+whitespace-characters+))
+            clap-builtin:+whitespace-characters+)
+  "a string composed of the whitespace characters.")
 
 (defconstant* +letters+
-  #.(concatenate 'string +lowercase+ +uppercase+))
+  #.(concatenate 'string +lowercase+ +uppercase+)
+  "a string composed of the letters. currently locale
+is not considered.")
 
 (defconstant* +printable+
   #.(concatenate 'string
                  +digits+
                  +letters+
                  +punctuation+
-                 +whitespace+))
+                 +whitespace+)
+  "a string composed of the printable characters. currently locale
+is not considered.")
 
