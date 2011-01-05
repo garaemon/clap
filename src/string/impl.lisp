d84d1c98c4ec39bdbfa46dc7b242d877155754f0
garaemon <garaemon@gmail.com>
2011-01-05 22:31:39 +0900

    re-implement capwords using functions in clap-builtin

--- src/string/impl.lisp
+++ src/string/impl.lisp
@@ -3,41 +3,36 @@
 ;; =============================================================================
 ;; string.capwords(s[, sep])
 ;; =============================================================================
-
 #||
 ;; in Python
 >>> string.capwords("foo bar^K baz")
 'Foo Bar Baz'
 >>> string.capwords("foo bar^K          baz")
 'Foo Bar Baz'
->>> string.capwords("foo bar baz" "")
+>>> string.capwords("foo bar baz" "") # not supported in clap, please use nil
 'Foo Bar Baz'
->>> string.capwords("foo bar             baz" "")
+>>> string.capwords("foo bar baz" " ")
+'Foo Bar Baz'
+>>> string.capwords("foo bar             baz" " ")
 'Foo Bar Baz'
 ||#
 
-(defgeneric capwords (obj &optional sep))
+(defun capwords (s &optional (sep " "))
+  "this is an implementation of string.capwords.
 
-(defmethod capwords ((string string) &optional (sep " " sep-p))
-  (let* ((sep (if (or (string= "" sep)
-                      (find (character sep) +whitespace+))
-                  " "
-                  sep))
-         (pat (ppcre:create-scanner
-               (if sep-p
-                   sep
-                   (sconc "[" +whitespace+ "]")))))
-    (reduce #'sconc
-            (loop :for word :on (delete "" (ppcre:split pat string) :test #'string=)
-                  :collect (string-capitalize (car word)) :into ans
-                  :if (null (cdr word))
-                    :return ans
-                  :else
-                    :collect sep :into ans))))
+capitalize the string S using the methods defined in clap-builtin.
 
-(defmethod capwords ((symbol symbol) &optional sep)
-  (values (intern (capwords (symbol-name symbol) sep))))
+first of all, split S using clap-builtin:split with SEP. SEP defaults to
+\" \".
 
+secondly, capitalize the each separated string using clap-builtin:capitalize.
+
+thirdly, joint the capitalized strings using clap-builtin:join."
+  (clap-builtin:join (or sep " ")
+                     (remove-if #'(lambda (x)
+                                    (string= x ""))
+                                (mapcar #'clap-builtin:capitalize
+                                        (clap-builtin:split s sep)))))
 
 ;; =============================================================================
 ;; string.maketrans(from, to)
