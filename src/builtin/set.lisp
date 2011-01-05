8aec32024bd3ca59d0b9008d529e2125cb88b330
garaemon <garaemon@gmail.com>
2011-01-05 23:11:36 +0900

    remove invalid comments

--- src/builtin/set.lisp
+++ src/builtin/set.lisp
@@ -68,27 +68,3 @@ return a copy of SET."))
 (defmethod copy ((set list))
   (copy-list set))
 
-;; ;; the methods for frozenset
-;; (defgeneric update (set other &key test)
-;;   (:documentation
-;;    "this is an implementation of fronzenset.update.
-
-;; adding the all elements of OTHER into SET.
-;; this return SET and performs as a destructive function."))
-
-;; (defmethod update ((set list) (other list) &key (test #'eql))
-;;   (loop
-;;      for element in other
-;;      if (not (cl:find element set :test test))
-;;      do (setf set (nconc set (list element))))
-;;   set)
-
-;; (defgeneric difference-update (set other &key test)
-;;   (:documentation
-;;    "this is an implementation of fronzenset.difference_update.
-
-;; update SET with removing elements found in OTHER"))
-
-;; (defmethod difference-update ((set list) (other list) &key (test #'eql))
-;;   (setf set (nset-difference set other :test test))
-;;   set)
