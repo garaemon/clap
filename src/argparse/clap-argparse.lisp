aae716165efb0def8159c6122a3f27960bbd6972
garaemon <garaemon@gmail.com>
2011-06-19 04:05:06 +0900

    export parse-args

--- src/argparse/clap-argparse.lisp
+++ src/argparse/clap-argparse.lisp
@@ -3,5 +3,6 @@
   (:documentation "clap-argparse is an implementation of argparse module")
   (:shadow #:prog #:type)
   (:export #:argument-parser
-           #:add-argument)
+           #:add-argument
+           #:parse-args)
   )
