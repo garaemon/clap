054242e002d08e2da00324277be35576ad4d4a3a
garaemon <garaemon@gmail.com>
2011-06-23 15:07:46 +0300

    add a sample

---
+++ src/argparse/samples/sample1.lisp
@@ -0,0 +1,23 @@
+(require :asdf)
+(setq asdf:*central-registry* '(#p"/Users/garaemon/kawauso/systems/"))
+
+(require :clap-argparse)
+
+(defvar *parser* (make-instance 'clap-argparse:argument-parser
+                              :description "Process some integers."))
+
+(clap-argparse:add-argument *parser* "integers"
+                            :metavar "N"
+                            :type 'int
+                            :nargs "+"
+                            :help "an integer for the accumulator")
+(clap-argparse:add-argument *parser* "--sum"
+                            :dest 'accumulate
+                            :action :store-const
+                            :const #'+
+                            :default #'max
+                            :help "sum the integers (default: find the max)")
+
+(defvar *args* (clap-argparse:parse-args *parser*))
+(print (apply (slot-value *args* 'accumulate)
+              (slot-value *args* 'integers)))
