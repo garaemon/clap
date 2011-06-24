5ddcc9ddb5724101429dea99d9cf068f25cd905c
garaemon <garaemon@gmail.com>
2011-06-24 18:23:42 +0300

    change to use function for argv rather than variable

--- src/sys/impl.lisp
+++ src/sys/impl.lisp
@@ -1,14 +1,14 @@
 (in-package :clap-sys)
 
-(defvar *argv*
-  #+allegro (system:command-line-arguments)
-  #+sbcl sb-ext:*posix-argv*
-  #+clisp ext:*args*
-  #+ecl (si:command-args)
-  #+cmu ext:*command-line-words*
-  #+ccl ccl:*command-line-argument-list*
-  #+lispworks system:*line-arguments-list*
-  "implementation of sys.argv. The list of command line arguments.")
+(defun argv ()
+    #+allegro (system:command-line-arguments)
+    #+sbcl sb-ext:*posix-argv*
+    #+clisp ext:*args*
+    #+ecl (si:command-args)
+    #+cmu ext:*command-line-words*
+    #+ccl ccl:*command-line-argument-list*
+    #+lispworks system:*line-arguments-list*
+    "implementation of sys.argv. The list of command line arguments.")
 
 (defun exit (code)
   #+sbcl (sb-unix:unix-exit code))
